8d7f0ce05ada83b2cccfb3cc59690669ecab0431
ssonsh <chosunci@gmail.com>
2022-03-14 08:53:55 +0900

    Service와 Repository를 활용하여 Aop 테스트를 진행하기 위해 AopTest 추가

---
+++ src/test/java/hello/aop/order/AopTest.java
@@ -0,0 +1,31 @@
+package hello.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(IllegalStateException.class);
+    }
+}
