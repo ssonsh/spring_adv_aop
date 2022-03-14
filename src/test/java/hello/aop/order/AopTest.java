a19738ed08734f9ec074676f6ac501b8df73b320
ssonsh <chosunci@gmail.com>
2022-03-15 07:25:20 +0900

    스프링 AOP Version 1 구성 및 적용

--- src/test/java/hello/aop/order/AopTest.java
+++ src/test/java/hello/aop/order/AopTest.java
@@ -1,13 +1,16 @@
 package hello.aop.order;
 
+import hello.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+@Import(AspectV1.class)
 @SpringBootTest
 public class AopTest {
 
@@ -22,6 +25,11 @@ public class AopTest {
     void aopInfo(){
         log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
         log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("ItemA");
     }
 
     @Test
