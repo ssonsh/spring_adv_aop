7d4f95198a319e5820ec2a6217d98a342ec0e1a5
ssonsh <chosunci@gmail.com>
2022-03-16 06:41:59 +0900

    스프링 AOP Version 6 구성 및 적용 - 종류별 어드바이스

--- src/test/java/hello/aop/order/AopTest.java
+++ src/test/java/hello/aop/order/AopTest.java
@@ -14,7 +14,8 @@ import org.springframework.context.annotation.Import;
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
 // @Import(AspectV4.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+// @Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 @SpringBootTest
 public class AopTest {
 
