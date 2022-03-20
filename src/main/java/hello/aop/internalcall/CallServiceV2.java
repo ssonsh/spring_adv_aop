a798e6807bc7ac08d0bede76b525cbd28513a9ad
ssonsh <chosunci@gmail.com>
2022-03-21 07:31:44 +0900

    스프링 AOP 적용 실무주의사항
    - 내부 호출 시 aop 적용 문제
    
    - ObjectProvider - 지연 조회

---
+++ src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,26 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public void external(){
+        log.info("call external");
+
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
