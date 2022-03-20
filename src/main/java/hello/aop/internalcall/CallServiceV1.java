47c8d94a7b4f407f307de73768213c7db2e60113
ssonsh <chosunci@gmail.com>
2022-03-21 07:04:36 +0900

    스프링 AOP 적용 실무주의사항 - 내부 호출 시 aop 적용 문제
    - 자기 자신 주입 (프록시 빈 주입)

---
+++ src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,29 @@
+package hello.aop.internalcall;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1){
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external(){
+        log.info("call external");
+
+        // 내부 메서드 호출 (this.internal());
+        callServiceV1.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
